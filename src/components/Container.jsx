4e2c7ecd543a1e30a5a8e68843d043b34d9abac4
Caio Maqueise Alécio Pinheiro <caio.maqueise@gmail.com>
2022-11-10 15:37:47 -0400

    🐛 💄 fix: scroll container

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -3,13 +3,14 @@ import styled from 'styled-components'
 const Div = styled.div`
     width: 100vw;
     height: 100vh;
-    background: linear-gradient(180deg, #17151F 0%, #1C172B 100%);
     position: relative;
     padding: 50px 25px 0 25px;
+    overflow-y: hidden;
+    background: linear-gradient(180deg, #17151F 0%, #1C172B 100%);
 `
 
-export function Container({children}){
-    return(
+export function Container({ children }) {
+    return (
         <Div>
             {children}
         </Div>
